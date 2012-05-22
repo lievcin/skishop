996ac058c7aa02c47f2c627078cce2cd2d4e55ce
Liev <liev.garcia@gmail.com>
2012-05-22 20:31:57 +0100

    maybe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ SkiRental::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
 
   # Generate digests for assets URLs
   config.assets.digest = true
